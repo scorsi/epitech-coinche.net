62f04db704bb0ae6bdb7349acdfb4e7c1a1c8469
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-11-04 14:53:49 +0100

    Add Protobuf Wrapper and Message class

---
+++ Lib/Sources/Protobuf/Wrapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Coinche.Protobuf
+{
+    public abstract class Wrapper
+    {
+        public enum Type
+        {
+            Unknown = 0,
+            Message = 1
+        };
+        
+        public Type ProtobufType { get; }
+        public byte[] ProtobufTypeAsBytes { get; }
+        
+        public Wrapper() {
+            var t = this.GetType();
+            if (t == typeof(Message))
+                ProtobufType = Type.Message;
+            else
+            {
+                ProtobufType = Type.Unknown;
+                throw new Exception("Object type unknown");
+            }
+            
+            ProtobufTypeAsBytes = BitConverter.GetBytes((short) ProtobufType);
+        }
+    }
+}
