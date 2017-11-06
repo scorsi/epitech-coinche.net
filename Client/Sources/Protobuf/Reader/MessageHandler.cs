327b76b3575d701e3708cabac1396974493c1bb4
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-11-06 18:18:53 +0100

    Add client Read & Write Message Handler

---
+++ Client/Sources/Protobuf/Reader/MessageHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Sockets;
+using Coinche.Protobuf;
+using Coinche.Protobuf.Reader;
+
+namespace Coinche.Client.Protobuf.Reader
+{
+    public class MessageHandler : IReader
+    {
+        public bool Run(NetworkStream stream, int clientId = 0)
+        {
+            var message = ProtoBuf.Serializer.DeserializeWithLengthPrefix<Message>(stream, ProtoBuf.PrefixStyle.Fixed32);
+
+            Console.Out.WriteLineAsync("Received message from server : " + message.Text);
+            
+            return true;
+        }
+    }
+}
---
+++ Client/Sources/Protobuf/Writer/MessageHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Sockets;
+using Coinche.Protobuf;
+using Coinche.Protobuf.Writer;
+
+namespace Coinche.Client.Protobuf.Writer
+{
+    public class MessageHandler : IWriter
+    {
+        public bool Run(NetworkStream stream, string input)
+        {
+            var message = new Message(input);
+            stream.Write(message.ProtobufTypeAsBytes, 0, 2);
+            ProtoBuf.Serializer.SerializeWithLengthPrefix(stream, message, ProtoBuf.PrefixStyle.Fixed32);
+            return true;
+        }
+    }
+}
