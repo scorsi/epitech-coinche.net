d376d974d3a445a3e77ad7444210c8ad01f609e7
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-11-07 15:24:20 +0100

    Add Invalid Arguments error message and fix a bug with same startwith aliases

--- Client/Sources/Inputs/InputManager.cs
+++ Client/Sources/Inputs/InputManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Linq;
 using System.Net.Sockets;
+using System.Text.RegularExpressions;
 using Coinche.Protobuf;
 using Coinche.Protobuf.Writer;
 
@@ -12,7 +13,7 @@ namespace Coinche.Client.Inputs
         /**
          * Hashtable which contains : Wrapper.Type & IProtobufReader 
          */
-        private Hashtable Table { get; set; } = new Hashtable();
+        private Hashtable Table { get; } = new Hashtable();
 
         /**
          * Status
@@ -22,20 +23,24 @@ namespace Coinche.Client.Inputs
         /**
          * Run
          */
-        public bool Run(NetworkStream ns, WriteManager wm, string input)
+        public bool Run(NetworkStream stream, WriteManager writeManager, string input)
         {
             try
             {
-                if (!input.StartsWith("/"))
-                    return wm.Run(ns, Wrapper.Type.Message, input);
+                if (!input.StartsWith("/")) 
+                    return writeManager.Run(stream, Wrapper.Type.Message, input);
                 
                 input = input.ToLower();
                 foreach (DictionaryEntry entry in Table)
                 {
-                    if (((string[]) entry.Key).Any(alias => input.StartsWith(alias)))
-                    {
-                        return wm.Run(ns, (Wrapper.Type) entry.Value, input);
-                    }
+                    if (!((string[]) entry.Key).Any(alias => Regex.Split(input, @"\s+")[0].Equals(alias)))
+                        continue;
+                    
+                    if (writeManager.Run(stream, (Wrapper.Type) entry.Value, input))
+                        return true;
+                    
+                    Console.Out.WriteLineAsync("Invalid Arguments");
+                    return false;
                 }
                 Console.Out.WriteLineAsync("Invalid Command");
                 return false;
@@ -65,7 +70,7 @@ namespace Coinche.Client.Inputs
         {
             Table.Add(inputs, type);
         }
-        
+
         /**
          * Add entries to Table
          */
