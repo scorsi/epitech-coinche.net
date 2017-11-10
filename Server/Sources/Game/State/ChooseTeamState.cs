ae142fb5ff7c5bdbb1203bc7407aae6b01113b05
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-11-10 22:53:53 +0100

    Add NextState, IsFinished and Initialize methods

--- Server/Sources/Game/State/ChooseTeamState.cs
+++ Server/Sources/Game/State/ChooseTeamState.cs
@@ -1,4 +1,11 @@
-﻿namespace Coinche.Server.Game.State
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Coinche.Protobuf;
+using Lib;
+using Lib.Game.Card;
+
+namespace Coinche.Server.Game.State
 {
     public class ChooseTeamState : AState
     {
@@ -8,20 +15,41 @@
 
         public override void Initialize()
         {
-            System.Console.Out.WriteLineAsync("ChooseTeam");
+            foreach (var client in Lobby.Info.Clients)
+            {
+                client.Team = Team.Undefined;
+            }
+
+            Lobby.Info.ResetPoints();
         }
 
         public override bool IsFinished()
         {
-            return false;
+            return Lobby.Info.Clients.All(client => client.Team != Team.Undefined);
         }
 
         public override AState NextState()
         {
+            var players = new List<ClientInfo>();
+            ClientInfo lastAddedPlayer = null;
+            
+            var lobbyPlayers = new List<ClientInfo>(Lobby.Info.Clients);
+            var random = new Random();
+
+            while (lobbyPlayers.Count != 0) {
+                var playerToAdd = lobbyPlayers[random.Next() * 100 % lobbyPlayers.Count];
+                
+                if (players.Count != 0 && playerToAdd.Team == lastAddedPlayer.Team) continue;
+                
+                players.Add(playerToAdd);
+                lastAddedPlayer = playerToAdd;
+                lobbyPlayers.Remove(playerToAdd);
+            }
+            
             return null;
         }
 
-        public override void HandleAction()
+        public override void HandleAction(Wrapper command, Client client)
         {
         }
     }
