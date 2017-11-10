01aad2921d66bcd50bed6a37f12b17275040d0c1
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-11-10 22:51:11 +0100

    Add Undefined Team

--- Lib/Sources/Game/Card/Team.cs
+++ Lib/Sources/Game/Card/Team.cs
@@ -1,22 +1,24 @@
 ﻿using System.Collections.Generic;
 
-namespace Lib.Sources.Game.Card
+namespace Lib.Game.Card
 {
     public class Team
     {
         public enum ETeam
         {
+            Undefined = -1,
             Red = 0,
             Blue = 1
         }
 
         private static readonly Dictionary<ETeam, Team> TeamMappings = new Dictionary<ETeam, Team>();
         
-        public static readonly Team RedTeam = new Team(ETeam.Red, "Red");
-        public static readonly Team BlueTeam = new Team(ETeam.Blue, "Blue");        
+        public static readonly Team Undefined = new Team(ETeam.Undefined, "Undefined");
+        public static readonly Team Red = new Team(ETeam.Red, "Red");
+        public static readonly Team Blue = new Team(ETeam.Blue, "Blue");        
 
-        private ETeam Index { get; }
-        private string Name { get; }
+        public ETeam Index { get; }
+        public string Name { get; }
 
         private Team(ETeam index, string name)
         {
