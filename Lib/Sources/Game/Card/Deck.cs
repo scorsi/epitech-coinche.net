bb5b6eb35c72028ec5e079002aee6f7d1878c1f7
maxencedcx <maxence.delacroix@epitech.eu>
2017-11-10 22:18:54 +0100

    Team, player, contract

---
+++ Lib/Sources/Game/Card/Deck.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Lib.Sources.Game.Card
+{
+    public class Deck
+    {
+
+        private List<Card> Cards { get; } = new List<Card>();
+
+        public int FoundCard(CardFace face, CardColor color)
+        {
+            int i = 0;
+            foreach (var card in Cards)
+            {
+                if (card.Face.Equals(face) && card.Color.Equals(color))
+                {
+                    return i;
+                }
+                ++i;
+            }
+            return -1;
+        }
+
+        public void AddCard(Card card)
+        {
+            Cards.Add(card);
+        }
+
+    }}
---
+++ Lib/Sources/Game/Card/Team.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace Lib.Sources.Game.Card
+{
+    public class Team
+    {
+        public enum ETeam
+        {
+            Red = 0,
+            Blue = 1
+        }
+
+        private static readonly Dictionary<ETeam, Team> TeamMappings = new Dictionary<ETeam, Team>();
+        
+        public static readonly Team RedTeam = new Team(ETeam.Red, "Red");
+        public static readonly Team BlueTeam = new Team(ETeam.Blue, "Blue");        
+
+        private ETeam Index { get; }
+        private string Name { get; }
+
+        private Team(ETeam index, string name)
+        {
+            Index = index;
+            Name = name;
+            TeamMappings[index] = this;
+        }
+
+        public static Team From(ETeam team)
+        {
+            return TeamMappings[team];
+        }
+
+        public static Team From(int id)
+        {
+            return TeamMappings[(ETeam) id];
+        }
+    }
+}
