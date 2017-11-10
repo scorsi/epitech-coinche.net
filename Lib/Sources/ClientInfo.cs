24d16f6d5d901afe50d1bc11de63663bae5c72c8
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-11-10 22:51:01 +0100

    Add Team information

--- Lib/Sources/ClientInfo.cs
+++ Lib/Sources/ClientInfo.cs
@@ -1,6 +1,7 @@
-﻿using ProtoBuf;
+﻿using Lib.Game.Card;
+using ProtoBuf;
 
-namespace Lib.Sources
+namespace Lib
 {
     [ProtoContract]
     public class ClientInfo
@@ -11,8 +12,28 @@ namespace Lib.Sources
         [ProtoMember(2)]
         public string Name { get; set; } = "";
 
+        [ProtoMember(3)]
+        public int TeamId
+        {
+            get => TeamId;
+
+            set
+            {
+                TeamId = value;
+                Team = Team.From(TeamId);
+            }
+        }
+
+        public Team Team
+        {
+            get => Team;
+
+            set => TeamId = (int) value.Index;
+        }
+
         public ClientInfo()
         {
+            TeamId = (int) Team.ETeam.Undefined;
         }
         
         public ClientInfo(int id)
