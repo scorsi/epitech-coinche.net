73b3575f75d81ce7d52ef9c9aae8a373580c95c0
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-11-12 00:56:23 +0100

    Add HandleContract

--- Server/Sources/Game/State/ContractState.cs
+++ Server/Sources/Game/State/ContractState.cs
@@ -1,4 +1,6 @@
-﻿using Coinche.Protobuf;
+﻿using System;
+using Coinche.Protobuf;
+using Lib;
 using Lib.Game.Card;
 
 namespace Coinche.Server.Game.State
@@ -17,6 +19,7 @@ namespace Coinche.Server.Game.State
             Lobby.Broadcast("You can now put contracts.");
             Turn = 0;
             Contract = null;
+            DisplayTurnMessage();
         }
 
         public override bool IsFinished()
@@ -34,9 +37,55 @@ namespace Coinche.Server.Game.State
 
         public override bool HandleAction(Wrapper command, Client client)
         {
-            if (command.ProtobufType != Wrapper.Type.LobbyContract) return false;
-            System.Console.Out.WriteLine("Yeah");
+            if (command.ProtobufType == Wrapper.Type.LobbyContract)
+            {
+                if (HandleContract((LobbyContract) command, client))
+                {
+                    ++Turn;
+                    DisplayTurnMessage();
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool HandleContract(LobbyContract command, Client client)
+        {
+            if (command.ContractType == ContractInfo.EType.Undefined || client.Info.Id != Lobby.Info.Clients[Turn].Id) return false;
+            
+            if (command.ContractType == ContractInfo.EType.Pass)
+            {
+                Lobby.Broadcast(client.Info.Name + " passed.");
+                return true;
+            }
+            if (Contract != null && Contract.Team == client.Info.Team)
+                return false;
+            if (command.ContractType == ContractInfo.EType.Coinche)
+            {
+                if (Contract == null)
+                    return false;
+                if (Contract.Type == ContractInfo.EType.Coinche)
+                    Lobby.Broadcast(client.Info.Name + " called recoinche.");
+            }
+            if (Contract?.Value <= command.ContractValue)
+                return false;
+
+            Contract = new Contract(client.Info.Team, command.ContractType, command.ContractValue);
+            Lobby.Broadcast(client.Info.Name + " put a contract " + command.ContractType + " of " +
+                            command.ContractValue + ".");
             return true;
+        }
+
+        private void DisplayTurnMessage()
+        {
+            try
+            {
+                Lobby.Broadcast("This is the turn of " + Lobby.Info.Clients[Turn].Name + " to put a contract.");
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
         }
     }
 }
