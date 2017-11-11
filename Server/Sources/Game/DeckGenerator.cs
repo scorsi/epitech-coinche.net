c02c091bef8ae2f33cc7e121602ff81b4d518be6
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-11-11 22:59:19 +0100

    Fix generate same cards

--- Server/Sources/Game/DeckGenerator.cs
+++ Server/Sources/Game/DeckGenerator.cs
@@ -11,8 +11,9 @@ namespace Coinche.Server.Game
         
         public static List<Deck> GenerateAllDecks() {
             var decks = new List<Deck>();
-            
-            for (var i = 0; i < 4; ++i) {
+
+            for (var i = 0; i < 4; ++i)
+            {
                 decks.Add(GenerateOneDeck(decks));
             }
             return decks;
@@ -20,26 +21,20 @@ namespace Coinche.Server.Game
         
         private static bool IsCardAvailable(List<Deck> pastDecks, Deck actualDeck, Card cardToCheck)
         {
-            return !(pastDecks.SelectMany(deck => deck.Cards).Any(card => card.Equals(cardToCheck)) || actualDeck.Cards.Any(card => card.Equals(cardToCheck)));
+            return !pastDecks.SelectMany(deck => deck.Cards).Any(card => card.Info.ColorId == cardToCheck.Info.ColorId && card.Info.FaceId == cardToCheck.Info.FaceId) && actualDeck.Cards.All(card => card.Info.ColorId != cardToCheck.Info.ColorId || card.Info.FaceId != cardToCheck.Info.FaceId);
         }
         
         private static Deck GenerateOneDeck(List<Deck> pastDecks) {
-            Console.Out.WriteLine("Generate One Deck");
-            
             var deck = new Deck();
             for (var i = 0; i < 8; ++i) {
-                Console.Out.WriteLine("Generate One Card");
-
                 Card card = null;
                 var isAvailable = false;
                 while (!isAvailable) {
 
-                    var faceId = Random.Next(1, 8);
-                    var colorId = Random.Next(1, 4);
+                    var faceId = Random.Next(2, 10);
+                    var colorId = Random.Next(2, 6);
                     var face = CardFace.From(faceId);
                     var color = CardColor.From(colorId);
-
-                    Console.Out.WriteLine("IsAvailable " + faceId + " " + colorId);
 
                     card = new Card(face, color);
                 
