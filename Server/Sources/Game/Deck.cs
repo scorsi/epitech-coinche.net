4ad4a32004947dcc4e0c4f39e6fe849cfb9fcea0
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-11-11 12:42:54 +0100

    Init the Card List

--- Server/Sources/Game/Deck.cs
+++ Server/Sources/Game/Deck.cs
@@ -7,6 +7,11 @@ namespace Coinche.Server.Game
     {
         public List<Card> Cards { get; set; }
 
+        public Deck()
+        {
+            Cards = new List<Card>();
+        }
+
         public void AddCard(Card card)
         {
             Cards.Add(card);
