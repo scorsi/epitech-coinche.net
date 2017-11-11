1739871ddf8deee2a5f2083995d151fdab4df0f5
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-11-11 12:43:36 +0100

    Add Contract State

--- Server/Sources/Game/State/ContractState.cs
+++ Server/Sources/Game/State/ContractState.cs
@@ -1,29 +1,40 @@
 ﻿using Coinche.Protobuf;
+using Lib.Game.Card;
 
 namespace Coinche.Server.Game.State
 {
     public class ContractState : AState
     {
+        private int Turn { get; set; }
+        private Contract Contract { get; set; }
+        
         public ContractState(Lobby lobby) : base("Contract", lobby)
         {
         }
 
         public override void Initialize()
         {
+            Lobby.Broadcast("You can now put contracts.");
+            Turn = 0;
+            Contract = null;
         }
 
         public override bool IsFinished()
         {
-            return false;
+            return Turn >= 4;
         }
 
         public override AState NextState()
         {
-            return null;
+            if (Contract != null)
+                return new GameState(Lobby);
+            Lobby.Broadcast("Anyone put contract. Replay the round.");
+            return new DrawState(Lobby);
         }
 
         public override void HandleAction(Wrapper command, Client client)
         {
+            ++Turn;
         }
     }
 }
