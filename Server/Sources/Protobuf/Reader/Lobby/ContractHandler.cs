651bb3002d29da51a07470c11c4ce3b2d7c46800
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-11-11 23:28:34 +0100

    Add ContractHandler writer and reader

---
+++ Server/Sources/Protobuf/Reader/Lobby/ContractHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Sockets;
+using Coinche.Protobuf;
+using Coinche.Protobuf.Reader;
+using Lib;
+
+namespace Coinche.Server.Protobuf.Reader.Lobby
+{
+    public class ContractHandler : IReader
+    {
+        public bool Run(NetworkStream stream, int clientId = 0)
+        {
+            var proto = ProtoBuf.Serializer.DeserializeWithLengthPrefix<LobbyContract>(stream, ProtoBuf.PrefixStyle.Fixed32);
+            var client = Server.Singleton.ClientList[clientId];
+            if (client.Lobby != null && client.Lobby.HandleAction(proto, client))
+            {
+                Server.Singleton.WriteManager.Run(stream, Wrapper.Type.LobbyContract,
+                    ((int) proto.ContractType).ToString());                
+            }
+            else
+            {
+                Server.Singleton.WriteManager.Run(stream, Wrapper.Type.LobbyContract,
+                    ((int) ContractInfo.EType.Undefined).ToString());
+            }
+            return true;
+        }
+    }
+}
---
+++ Server/Sources/Protobuf/Writer/Lobby/ContractHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Sockets;
+using Coinche.Protobuf;
+using Coinche.Protobuf.Writer;
+using Lib;
+
+namespace Coinche.Server.Protobuf.Writer.Lobby
+{
+    public class ContractHandler : IWriter
+    {
+        public bool Run(NetworkStream stream, string input)
+        {
+            var proto = new LobbyContract {ContractType = (ContractInfo.EType) Convert.ToInt32(input)};
+            stream.Write(proto.ProtobufTypeAsBytes, 0, 2);
+            ProtoBuf.Serializer.SerializeWithLengthPrefix(stream, proto, ProtoBuf.PrefixStyle.Fixed32);
+            return true;
+        }
+    }
+}
