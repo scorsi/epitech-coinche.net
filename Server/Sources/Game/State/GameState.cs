d44396b42876b1cda86c3dd18df54ab5eaaa0c2f
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-11-11 12:12:12 +0100

    Add GameState

---
+++ Server/Sources/Game/State/GameState.cs
@@ -0,0 +1,29 @@
+﻿using Coinche.Protobuf;
+
+namespace Coinche.Server.Game.State
+{
+    public class GameState : AState
+    {
+        public GameState(Lobby lobby) : base("Game", lobby)
+        {
+        }
+
+        public override void Initialize()
+        {
+        }
+
+        public override bool IsFinished()
+        {
+            return false;
+        }
+
+        public override AState NextState()
+        {
+            return null;
+        }
+
+        public override void HandleAction(Wrapper command, Client client)
+        {
+        }
+    }
+}
