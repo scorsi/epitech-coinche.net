dce02d1924ca0d43a660a193333d1eb0fec479f7
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-11-11 23:28:59 +0100

    Handle return value of HandleAction

--- Server/Sources/Protobuf/Reader/Lobby/TeamHandler.cs
+++ Server/Sources/Protobuf/Reader/Lobby/TeamHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net.Sockets;
 using Coinche.Protobuf;
 using Coinche.Protobuf.Reader;
+using Lib.Game.Card;
 
 namespace Coinche.Server.Protobuf.Reader.Lobby
 {
@@ -10,8 +11,14 @@ namespace Coinche.Server.Protobuf.Reader.Lobby
         {
             var proto = ProtoBuf.Serializer.DeserializeWithLengthPrefix<LobbyTeam>(stream, ProtoBuf.PrefixStyle.Fixed32);
             var client = Server.Singleton.ClientList[clientId];
-            client.Lobby?.HandleAction(proto, client);
-            Server.Singleton.WriteManager.Run(stream, Wrapper.Type.LobbyTeam, client.Info.TeamId.ToString());
+            if (client.Lobby != null && client.Lobby.HandleAction(proto, client))
+            {
+                Server.Singleton.WriteManager.Run(stream, Wrapper.Type.LobbyTeam, client.Info.TeamId.ToString());                
+            }
+            else
+            {
+                Server.Singleton.WriteManager.Run(stream, Wrapper.Type.LobbyTeam, ((int) Team.ETeam.Undefined).ToString());
+            }
             return true;
         }
     }
